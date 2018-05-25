2f00b8b8a3e1e77ede5b31654b79b0ec8f872fa9
xc1618 <xchen1996@gmail.com>
2018-05-25 13:57:22 +0800

    fix a bug

--- app/src/main/java/com/xc161/Android_2048_app/GameActivity.java
+++ app/src/main/java/com/xc161/Android_2048_app/GameActivity.java
@@ -58,7 +58,7 @@ public class GameActivity extends AppCompatActivity implements my2048.OnGameList
     @Override
     public void OnGameOver() {
         if (sharedPreferences.getInt("max", 0) < Integer.valueOf(scoreview.getText().toString().substring(6))) {
-            editor.putInt("max", Integer.valueOf(scoreview.getText().toString()));
+            editor.putInt("max", Integer.valueOf(scoreview.getText().toString().substring(6)));
             editor.commit();
         }
         new AlertDialog.Builder(this).setTitle("Game Over").setMessage("You Have Got" + scoreview.getText()).setPositiveButton("Restart", new DialogInterface.OnClickListener() {
